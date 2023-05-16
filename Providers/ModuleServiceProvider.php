147684d0d6450aa9648c1fd7e0aceb1245b71100
TohurTV <joshwebb84@outlook.com>
2023-05-16 16:53:43 -0600

    First Commit

---
+++ Providers/ModuleServiceProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Modules\Like\Providers;
+
+use Konekt\Concord\BaseModuleServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Schema;
+
+class ModuleServiceProvider extends BaseModuleServiceProvider
+{
+    /**
+     * The namespace for the module's models.
+     *
+     * @var string
+     */
+    protected $modelNamespace = 'Modules\Like\Models';
+
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Your module's boot logic here
+        $this->app->register(RouteServiceProvider::class);
+        $this->app->register(PluginServiceProvider::class);
+        $this->ViewPaths();
+        $this->adminViewPaths();
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Your module's register logic here
+    }
+    
+    public function ViewPaths() {
+        $moduleLower = lcfirst('Like');
+        if (Schema::hasTable('settings')) {
+            $setting = DB::table('settings')->where('id', 'site.theme')->first();
+            $currentTheme = $setting->value;
+        } else {
+            $currentTheme = 'default';
+        }
+        $views = [
+            base_path("themes/$currentTheme/views/modules/Like"),
+            module_Viewpath('Like', $currentTheme),
+            base_path("themes/default/views/modules/Like"),
+            module_Viewpath('Like', 'default'),
+            base_path("resources/views/modules/Like"),
+        ];
+
+        return $this->loadViewsFrom($views, $moduleLower);
+    }
+
+    public function adminViewPaths() {
+        $moduleLower = lcfirst('Like');
+        $currentTheme = 'admin';
+        $views = [
+            module_Viewpath('Like', $currentTheme),
+            base_path("themes/$currentTheme/views/modules/Like"),
+        ];
+
+        return $this->loadViewsFrom($views, $moduleLower.'-admin');
+    }
+}
+
---
+++ resources/manifest.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'name'    => 'Like',
+    'version' => '0.1.0'
+];
+
